25328a94545fe9b73ffe73b31150a3681e8c4b77
hexu <hxbcwj@163.com>
2019-02-24 08:52:16 +0800

    Ver 2.4

--- IndividualWorks/Blog/views.py
+++ IndividualWorks/Blog/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response,render,HttpResponseRedirect
 from Blog.models import *
 
 def index(request):
     blogsright = BlogArticle.objects.all()[::-1][:7]
-    blogsleft = BlogArticle.objects.all()[0:7]
+    blogsleft = BlogArticle.objects.all()[:7]
     myblogs = BlogArticle.objects.all()[::-1]
     return render(request,'blog/index.html',locals())
 
 def addblog(request):
     blogsright = BlogArticle.objects.all()[::-1][:7]
-    blogsleft = BlogArticle.objects.all()[0:7]
+    blogsleft = BlogArticle.objects.all()[:7]
     if request.method == 'POST' and request.POST:
         ab = BlogArticle()
         ab.title = request.POST.get('title')
@@ -22,7 +22,7 @@ def addblog(request):
 
 def blogs(request,num):
     blogsright = BlogArticle.objects.all()[::-1][:7]
-    blogsleft = BlogArticle.objects.all()[0:7]
+    blogsleft = BlogArticle.objects.all()[:7]
     num = int(num)
     myblog = BlogArticle.objects.filter(id = num).first()
     return render(request, 'blog/blog.html', locals())
@@ -30,6 +30,6 @@ def blogs(request,num):
 
 def blogslist(request):
     blogsright = BlogArticle.objects.all()[::-1][:7]
-    blogsleft = BlogArticle.objects.all()[0:7]
+    blogsleft = BlogArticle.objects.all()[:7]
     myblogs = BlogArticle.objects.all()[::-1]
     return render(request, 'blog/bloglist.html',  locals())
